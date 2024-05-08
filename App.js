b8690f0ada37ff2cc1810304480dbb9269459215
John Louie Antoque <androidantoque@gmail.com>
2024-05-08 17:39:09 +0800

    add read me

--- App.js
+++ App.js
@@ -41,7 +41,7 @@ export default function App() {
     
     const resData = restructureJson(sampleData)
 
-    console.log("---------",resData)
+    console.log("restructure data ---------",resData)
     
 
   return (
